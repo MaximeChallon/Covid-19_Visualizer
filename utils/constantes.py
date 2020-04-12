3fa61b1f7553cf1363ed67d5d4f15c804122d2da
Maxime Challon <maxime.challon@gmail.com>
2020-04-12 11:10:11 +0200

    constantes des datasets

---
+++ utils/constantes.py
@@ -0,0 +1,6 @@
+# datasets from John Hopkins University
+DATA_DEATHS_GLOBAL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
+DATA_DEATHS_US = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
+DATA_CONFIRMED_GLOBAL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+DATA_CONFIRMED_US = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+DATA_RECOVERED_GLOBAL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
