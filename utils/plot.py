57b04cb5d11b2894f39da113331bd9f21deaa1f4
Maxime Challon <maxime.challon@gmail.com>
2020-04-14 09:35:16 +0200

    gestion des dates sur l'axe x

--- utils/plot.py
+++ utils/plot.py
@@ -5,19 +5,16 @@ import streamlit as st
 import numpy as np
 
 
-def get_features(data) :
+def get_features_general(data) :
     feature_data = data.drop(columns=[
             "Date",
             "Country",
-            "Population"])
+            "Population",
+            "Percent_Change_Cases",
+            "Percent_Change_Deaths",
+            "New_Cases_per_10000",
+            "New_Deaths_per_10000"])
     return feature_data.columns.tolist()
-
-
-def calculate_growth_factor(data, features, prefix: str = "growth_factor"):
-    for feature in features:
-        data[f"{feature}_yesterday"] = data[feature].shift()
-        data[f"{prefix}_{feature}"] = data[feature] / data[f"{feature}_yesterday"]
-    return data
 
 
 def generate_global_chart(
@@ -37,7 +34,7 @@ def generate_global_chart(
             y=alt.Y(f"{feature}", title=feature, scale=scale),
             tooltip=[
                 alt.Tooltip(f"{feature}", title=feature),
-                alt.Tooltip("Date", title=x_title),
+                alt.Tooltip("Date", title=x_title, type="temporal"),
             ],
         )
         .configure_scale(continuousPadding=padding)
@@ -47,17 +44,15 @@ def generate_global_chart(
 
 
 def line_plots(data):
-    features = get_features(data)
+    features = get_features_general(data)
     feature = st.selectbox(label=("Choose..."), options=features)
 
     # Group data by date
-    gf_prefix = ("growth_factor")
     general = data.groupby("Date", as_index=False).sum()
-    general = calculate_growth_factor(general, features, prefix=gf_prefix)
 
     # Choose log scale or linear, defines what feature to use
-    general_choice = st.radio(label=("Scale"), options=[("linear"), ("logarithmic")])
-    if general_choice == ("logarithmic"):
+    general_choice = st.radio(label=("Scale"), options=[("Linéaire"), ("Logarithmique")])
+    if general_choice == ("Logarithmique"):
         general = general[general[feature] > 0]
         general_scale = alt.Scale(type="log")
     else:
